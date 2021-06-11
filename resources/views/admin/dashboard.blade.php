efeb54b617c59515b064c5a43850142e567c4753
Mhmd Hasan <mhmdhasan@users.noreply.github.com>
2021-06-11 18:02:37 +0200

    Update the forms help text

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,7 +9,7 @@
             <div class="card rounded-lg card-striped card-striped-gradient-success">
                 <div class="card-body rounded-lg bg-white shadow-sm p-lg-4">
                     <h2 class="h5 text-uppercase letter-spacing-0 mb-0 fw-bold">{{__('All subscribers')}}</h2>
-                    <p class="text-gray-500 mb-4">{{__('Users that confirmed their emails')}}</p>
+                    <p class="text-gray-500 mb-4">{{__('All users that have subscribed to get notified.')}}</p>
                     <p class="h1 mb-0 fw-bold text-success">{{ $all_users }}</p>
                 </div>
             </div>
@@ -20,7 +20,7 @@
             <div class="card rounded-lg card-striped card-striped-gradient-danger">
                 <div class="card-body rounded-lg bg-white shadow-sm p-lg-4">
                     <h2 class="h5 text-uppercase letter-spacing-0 mb-0 fw-bold">{{__('New subscribers')}}</h2>
-                    <p class="text-gray-500 mb-4">{{__('Users that haven\'t')}}</p>
+                    <p class="text-gray-500 mb-4">{{__('New subscribers this month.')}}</p>
                     <p class="h1 mb-0 fw-bold text-danger">{{ $new_users }}</p>
                 </div>
             </div>
@@ -31,7 +31,7 @@
             <div class="card rounded-lg card-striped card-striped-gradient-info">
                 <div class="card-body rounded-lg bg-white shadow-sm p-lg-4">
                     <h2 class="h5 text-uppercase letter-spacing-0 mb-0 fw-bold">{{__('Page views')}}</h2>
-                    <p class="text-gray-500 mb-4">{{__('All the subscribers all time')}}</p>
+                    <p class="text-gray-500 mb-4">{{__('How many users see your landing page.')}}</p>
                     <p class="h1 mb-0 fw-bold text-info">{{ $views }}</p>
                 </div>
             </div>
