082fff13fe0268d903fb7df9620ed50aa601e01e
Mhmd Hasan <mhmdhasan@users.noreply.github.com>
2021-06-11 18:19:19 +0200

    Update form help text

--- resources/views/admin/general.blade.php
+++ resources/views/admin/general.blade.php
@@ -30,63 +30,63 @@
             <div class="form-group col-lg-6">
                 <!-- Input-->
                 <label class="form-label h5 mb-0 required" for="themeHeading">{{__('Theme main heading')}}</label>
-                <p class="form-text mb-3">{{__('Lorem ipsum dolor sit amet, consectetur adipisicing.')}}</p>
+                <p class="form-text mb-3">{{__('The large heading appears on counter\'s page center.')}}</p>
                 <input class="form-control shadow-0" id="themeHeading" type="text" name="main_heading" placeholder="Type theme main heading" value="{{ $generalSetting->main_heading ?? '' }}" required="required" />
             </div>
 
             <div class="form-group col-lg-6">
                 <!-- Input-->
                 <label class="form-label h5 mb-0 required" for="newsLetterText">{{__('Newsletter text')}}</label>
-                <p class="form-text mb-3">{{__('Lorem ipsum dolor sit amet, consectetur adipisicing.')}}</p>
+                <p class="form-text mb-3">{{__('The newsletter\'s text message encourages users to subscribe.')}}</p>
                 <input class="form-control shadow-0" id="newsLetterText" type="text" name="newsletter" placeholder="Type newsletter notifying text" value="{{ $generalSetting->newsletter ?? '' }}" required="required" />
             </div>
 
             <div class="form-group col-lg-6">
                 <!-- Input-->
                 <label class="form-label h5 mb-0 required" for="submitText">{{__('Submit button text')}}</label>
-                <p class="form-text mb-3">{{__('Lorem ipsum dolor sit amet, consectetur adipisicing.')}}</p>
+                <p class="form-text mb-3">{{__('The text value of newsletter\'s submit button.')}}</p>
                 <input class="form-control shadow-0" id="submitText" type="text" name="submit_button" placeholder="Type newsletter notifying text" value="{{ $generalSetting->submit_button ?? '' }}" required="required" />
             </div>
 
             <div class="form-group col-lg-6">
                 <!-- Input-->
                 <label class="form-label h5 mb-0 required" for="copyrightsText">{{__('Copyrights text')}}</label>
-                <p class="form-text mb-3">{{__('Lorem ipsum dolor sit amet, consectetur adipisicing.')}}</p>
+                <p class="form-text mb-3">{{__('Page\'s footer copyrights message.')}}</p>
                 <input class="form-control shadow-0" id="copyrightsText" type="text" name="copyrights" placeholder="Type copyrights text" value="{{ $generalSetting->copyrights ?? '' }}" required="required" />
             </div>
 
             <div class="form-group col-lg-6">
                 <!-- Input-->
                 <label class="form-label h5 mb-0 required" for="pageName">{{__('Page name')}}</label>
-                <p class="form-text mb-3">{{__('Lorem ipsum dolor sit amet, consectetur adipisicing.')}}</p>
+                <p class="form-text mb-3">{{__('The name you can see on the browser\'s tab... This value is a <meta> tag value.')}}</p>
                 <input class="form-control shadow-0" id="pageName" type="text" name="page_name" placeholder="Set your page title" value="{{ $generalSetting->page_name ?? '' }}" required="required" />
             </div>
 
             <div class="form-group col-lg-6">
                 <!-- Input-->
                 <label class="form-label h5 mb-0" for="metaKeywords">{{__('Meta keywords')}}</label>
-                <p class="form-text mb-3">{{__('Lorem ipsum dolor sit amet, consectetur adipisicing.')}}</p>
+                <p class="form-text mb-3">{{__('Type some keywords relative to your released project... This value is a <meta> tag value.')}}</p>
                 <input class="form-control shadow-0" id="metaKeywords" type="text" name="meta_keywords" placeholder="Enter your page keywords" value="{{ $generalSetting->meta_keywords ?? '' }}" />
             </div>
 
             <div class="form-group col-lg-6">
                 <!-- Input-->
                 <label class="form-label h5 mb-0" for="metaAuthor">{{__('Meta author')}}</label>
-                <p class="form-text mb-3">{{__('Lorem ipsum dolor sit amet, consectetur adipisicing.')}}</p>
+                <p class="form-text mb-3">{{__('Type the author name of your released project... This value is a <meta> tag value.')}}</p>
                 <input class="form-control shadow-0" id="metaAuthor" type="text" name="meta_author" placeholder="Set your page author" value="{{ $generalSetting->meta_author ?? '' }}" />
             </div>
 
             <div class="form-group col-lg-6">
                 <!-- Input-->
                 <label class="form-label h5 mb-0" for="pageFavicon">{{__('Favicon')}}</label>
-                <p class="form-text mb-3">{{__('Lorem ipsum dolor sit amet, consectetur adipisicing.')}}</p>
+                <p class="form-text mb-3">{{__('upload the browser\'s tab favicon, the image uploaded dimensions must be 80x80px.')}}</p>
                 <input class="form-control shadow-0" id="pageFavicon" type="file" name="favicon_image" />
             </div>
 
             <div class="form-group col-lg-6">
                 <!-- Input-->
                 <label class="form-label h5 mb-0" for="metaDescription">{{__('Timezone')}}</label>
-                <p class="form-text mb-3">{{__('Lorem ipsum dolor sit amet, consectetur adipisicing.')}}</p>
+                <p class="form-text mb-3">{{__('Add a short brief about your released project... This value is a <meta> tag value.')}}</p>
                 <select class="form-select shadow-0" name="timezone">
                   @foreach ($timeZones as $key => $value)
                     <option value="{{ $value }}" {{ $generalSetting->timezone == $value ? 'selected' : ''}}>{{ $value }}</option>
