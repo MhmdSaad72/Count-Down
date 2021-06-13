b0f837da8a8e33dde6bafd9fc4bce542d78ee1d5
Mhmd Hasan <mhmdhasan@users.noreply.github.com>
2021-06-13 22:44:37 +0200

    add coming soon message

--- resources/views/themes/theme-one.blade.php
+++ resources/views/themes/theme-one.blade.php
@@ -86,7 +86,12 @@
     <section class="py-5 text-center">
         <div class="container pt-5">
             <!-- Page main heading-->
-            <h1 class="theme-1-heading heading-4x text-uppercase text-shadow mb-5">{{ $generalSetting->main_heading ?? '' }}</h1>
+            <h1 class="theme-1-heading heading-4x text-uppercase text-shadow mb-2">{{ $generalSetting->main_heading ?? '' }}</h1>
+            <div class="row">
+              <div class="col-lg-8 mx-auto">
+                <p class="mb-5 lead">{{ $generalSetting->counter_message ?? ''}}</p>
+              </div>
+            </div>
             <!-- Counter-->
             <div class="counter" id="counter">
                 <div class="mx-1">
